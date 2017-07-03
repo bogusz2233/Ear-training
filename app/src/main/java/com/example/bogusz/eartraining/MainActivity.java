b20d71b35168ae49832d3720000830ef54d4076e
bogusz2233 <bogusz2233@gmail.com>
2017-07-03 15:09:30 +0200

    fixed main layout(mainActivity) and fragment(IkonaFragment)
    fixed table, described column and row

--- app/src/main/java/com/example/bogusz/eartraining/MainActivity.java
+++ app/src/main/java/com/example/bogusz/eartraining/MainActivity.java
@@ -17,6 +17,7 @@ public class MainActivity extends AppCompatActivity implements IkonaFragment.OnF
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         loadPhoto();
